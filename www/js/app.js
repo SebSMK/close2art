46e6fb2a5abcc5e1d9b5b00a018d72a9f4005654
unknown <sbr@PC-0076.SMK.DK>
2016-11-17 14:46:01 +0100

    [FEATURE] openseadragonselection added

--- www/js/app.js
+++ www/js/app.js
@@ -85,9 +85,9 @@
               id:                 "dz",              
               prefixUrl: "img/",
              	//constrainDuringPan: true,
-             	zoomInButton:   "zoom-in",
-             	zoomOutButton:  "zoom-out",
-             	fullPageButton: "zoom-expand",
+             	//zoomInButton:   "zoom-in",
+             	//zoomOutButton:  "zoom-out",
+             	//fullPageButton: "zoom-expand",
              	//sequenceMode: true,
              	//showSequenceControl: false,
              	maxZoomPixelRatio: 1.2,
@@ -96,7 +96,46 @@
               minZoomLevel:       1,
              	homeFillsViewer: true,
               tileSources:   [response]
-          });  
+          }); 
+          
+          var selection = viewer.selection({
+            element:                 null, // html element to use for overlay
+            showSelectionControl:    true, // show button to toggle selection mode
+            toggleButton:            null, // dom element to use as toggle button
+            showConfirmDenyButtons:  true,
+            styleConfirmDenyButtons: true,
+            returnPixelCoordinates:  true,
+            keyboardShortcut:        'c', // key to toggle selection mode
+            rect:                    null, // initial selection as an OpenSeadragon.SelectionRect object
+            startRotated:            false, // alternative method for drawing the selection; useful for rotated crops
+            startRotatedHeight:      0.1, // only used if startRotated=true; value is relative to image height
+            restrictToImage:         false, // true = do not allow any part of the selection to be outside the image
+            onSelection:             function(rect) {
+                                          alert(rect + ' Center point: ' + rect.getCenter() + ' Degree rotation: ' + rect.getDegreeRotation());
+                                      },
+            prefixUrl:               null, // overwrites OpenSeadragon's option
+            navImages:               { // overwrites OpenSeadragon's options
+                selection: {
+                    REST:   'selection_rest.png',
+                    GROUP:  'selection_grouphover.png',
+                    HOVER:  'selection_hover.png',
+                    DOWN:   'selection_pressed.png'
+                },
+                selectionConfirm: {
+                    REST:   'selection_confirm_rest.png',
+                    GROUP:  'selection_confirm_grouphover.png',
+                    HOVER:  'selection_confirm_hover.png',
+                    DOWN:   'selection_confirm_pressed.png'
+                },
+                selectionCancel: {
+                    REST:   'selection_cancel_rest.png',
+                    GROUP:  'selection_cancel_grouphover.png',
+                    HOVER:  'selection_cancel_hover.png',
+                    DOWN:   'selection_cancel_pressed.png'
+                },
+            }
+        });
+          
       })
       .fail(function(err) {
         onFail(err);  
